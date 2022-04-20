72f2458c5926d6b6a692f071cf2abf470b864908
zhangfan0319 <49545739+zhangfan0319@users.noreply.github.com>
2022-04-20 11:33:43 +0800

    Create multiple_trait.sh

---
+++ multiple_trait.sh
@@ -0,0 +1,7 @@
+#### shell 整合脚本 
+Rscritp Gmatrix.R 
+for i in bw42_g sfw_g sfr_% afw_g afr_% bmw_g bmr_% bmt_mm sf_N fac_% sft_mm ew dw fbl bw neck_len bl fcr rfi;
+do
+sed  s/trait/$i/g gblup.R > ${i}.R
+Rscript ${i}.R
+done
