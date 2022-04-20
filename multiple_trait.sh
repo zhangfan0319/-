fad7a2dad5fcac62fb5562c5e286e17e7aec60c0
zhangfan0319 <49545739+zhangfan0319@users.noreply.github.com>
2022-04-20 11:50:41 +0800

    Update multiple_trait.sh

--- multiple_trait.sh
+++ multiple_trait.sh
@@ -3,5 +3,6 @@ Rscritp Gmatrix.R
 for i in bw42_g sfw_g sfr_% afw_g afr_% bmw_g bmr_% bmt_mm sf_N fac_% sft_mm ew dw fbl bw neck_len bl fcr rfi;
 do
 sed  s/trait/$i/g gblup.R > ${i}.R
-Rscript ${i}.R
+Rscript ${i}.R &
 done
+wait
